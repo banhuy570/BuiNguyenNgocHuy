9c29870d081c7babf4b4c1fb48df87b58a141fa6
banhuy570 <71781815+banhuy570@users.noreply.github.com>
2020-09-30 22:23:18 +0700

    sửa

--- signin/src/main/java/huy/com/signin/main.java
+++ signin/src/main/java/huy/com/signin/main.java
@@ -1,13 +1,26 @@
 package huy.com.signin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class main extends AppCompatActivity {
+    private Button btnlogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        btnlogout = (Button) findViewById(R.id.btlogout);
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(main.this, SignIn.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
